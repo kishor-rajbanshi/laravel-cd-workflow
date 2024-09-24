092ff76cce32d53b1f6824fdcf51087cce73cfc7
Kishor Rajbanshi <kishorrajbanshi00@gmail.com>
2024-09-24 19:09:05 +0000

    add docker deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,10 +4,30 @@ set -e
 
 echo "Starting deployment..."
 
+file="store.txt"
+
+generate_random_string() {
+    tr -dc 'a-z0-9-' < /dev/urandom | head -c 32
+}
+
 if [ -n "$1" ]; then
     if [[ "$(basename "$1")" == "Dockerfile" ]]; then
-       docker build --no-cache -f "$1" .
-    else [[ "$(basename "$1")" == "docker-compose.yml" ]]; then
+
+        if [[ ! -f "$file" ]]; then
+            echo "image_name:$(generate_random_string)" > "$file"
+            echo "container_name:$(generate_random_string)" >> "$file"
+        else
+            while IFS=":" read -r key value; do
+                declare "$key=$value"
+            done < "$file"
+        fi
+
+        docker build -t "$image_name" --no-cache -f "$1" .
+        (docker stop "$container_name") || true
+        (docker rm "$container_name") || true
+        docker run -d --name "$container_name" "$image_name"
+
+    elif [[ "$(basename "$1")" == "docker-compose.yml" ]]; then
         docker compose -f "$1" build --no-cache
         docker compose -f "$1" down
         docker compose -f "$1" up -d
